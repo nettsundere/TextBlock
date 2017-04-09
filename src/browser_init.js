858d39465594ce489a8a08213d0e28b01f3d5cb0
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2017-04-09 11:34:16 +0300

    Optimize app status loading

--- src/browser_init.js
+++ src/browser_init.js
@@ -1,27 +1,23 @@
 var application = new App();
 var ui = new UI();
 
-application.Init(function() {
+application.Init(function(appIsEnabled) {
+  var isCurrentlyEnabled = appIsEnabled;
   var pageInjector = new PageInjector(chrome);
 
-  application.GetEnabled(function(isCurrentlyEnabled) {
-    pageInjector.ToggleCSS(isCurrentlyEnabled);
-  });
-
   var appHandler = function() { 
-    application.GetEnabled(function(isCurrentlyEnabled) {
-      ui.ToggleInterfaceState(isCurrentlyEnabled, function() {
-        pageInjector.ToggleCSS(isCurrentlyEnabled);
-      });
+    ui.ToggleInterfaceState(isCurrentlyEnabled, function() {
+      pageInjector.ToggleCSS(isCurrentlyEnabled);
     });
   };
 
+  appHandler();
+
   var toggleHandler = function(tab) {
-    application.GetEnabled(function(isCurrentlyEnabled) {
-      application.SetEnabled(!isCurrentlyEnabled, function(isActive) {
-        ui.ToggleInterfaceState(isActive, function() {
-          pageInjector.ToggleCSS(isActive);
-        });
+    isCurrentlyEnabled = !isCurrentlyEnabled;
+    application.SetEnabled(isCurrentlyEnabled, function(isActive) {
+      ui.ToggleInterfaceState(isActive, function() {
+        pageInjector.ToggleCSS(isActive);
       });
     });
   };
