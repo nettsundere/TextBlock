d56958436d0455c2449810c52d1089476ab53f5b
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2017-04-09 11:46:23 +0300

    Comment ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,35 +1,54 @@
+/**
+ * User interface service.
+ */
 var UI = function() {
+  // Icons states
   this.icons = {
-    false: {
+    false: { // App is disabled
       "16": "icons/disabled/icon16.png",
       "24": "icons/disabled/icon24.png",
       "32": "icons/disabled/icon32.png"
     },
-    true: {
+    true: { // App is enabled
       "16": "icons/enabled/icon16.png",
       "24": "icons/enabled/icon24.png",
       "32": "icons/enabled/icon32.png"
     }
   };
 
+  // Tooltips states
   this.tooltips = {
-    false: "Click to hide all text",
-    true: "Click to show all text"
+    false: "Click to hide all text", // App is disabled
+    true: "Click to show all text" // App is enabled
   };
 };
 
+/**
+ * Toggle between app ui icon states.
+ * @param isEnabled - bool value indicating whether the app is enabled.
+ * @param callback - the callback to handle app ui state change.
+ */
 UI.prototype._ToggleIcons = function(isEnabled, callback) {
   chrome.browserAction.setIcon({
     path: this.icons[isEnabled]
   }, callback);
 };
 
+/**
+ * Toggle between app ui tooltips states.
+ * @param isEnabled - bool value indicating whether the app is enabled.
+ */
 UI.prototype._ToggleTooltips = function(isEnabled) {
   chrome.browserAction.setTitle({
     title: this.tooltips[isEnabled]
   });
 };
 
+/**
+ * Toggle the interface state of the app.
+ * @param isEnabled - bool value indicating whether the app is enabled.
+ * @param callback - the callback to handle app ui state change.
+ */
 UI.prototype.ToggleInterfaceState = function(isEnabled, callback) {
   var that = this;
   that._ToggleIcons(isEnabled, function() {
